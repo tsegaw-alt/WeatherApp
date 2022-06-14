b878c7d16473b3145f672b9860892b8ebfe23f29
tsegaw <grace.d224@yahoo.com>
2022-06-14 11:58:37 +0300

    replace static imports with dynamic one

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,9 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import Home from '../views/Home.vue'
-
-const routes = [
-  {path: '/', name: 'Home', component: Home},
-  // {path: '/', name: 'Home', component: Home},
-
-]
+import { createRouter, createWebHistory } from "vue-router";
+const Home = () => import("../views/Home.vue");
+const routes = [{ path: "/", name: "Home", component: Home }];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
-export default router 
+  routes,
+});
+export default router;
